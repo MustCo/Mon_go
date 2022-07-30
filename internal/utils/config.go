efc4b84deab3e4b7639ff4a8fa3f720b4cd0fa0d
Snakder <kamojabishvili@gmail.com>
2022-07-30 14:01:36 +0300

    изменил стандартное значение в интервалах

--- internal/utils/config.go
+++ internal/utils/config.go
@@ -4,8 +4,8 @@ import "time"
 
 type Config struct {
 	Address        string        `env:"ADDRESS,required" envDefault:"127.0.0.1:8080" json:"ADDRESS"`
-	PollInterval   time.Duration `env:"POLL_INTERVAL" envDefault:"2" json:"POLL_INTERVAL"`
-	ReportInterval time.Duration `env:"REPORT_INTERVAL"  envDefault:"10" json:"REPORT_INTERVAL"`
+	PollInterval   time.Duration `env:"POLL_INTERVAL" envDefault:"2s" json:"POLL_INTERVAL"`
+	ReportInterval time.Duration `env:"REPORT_INTERVAL" envDefault:"10s" json:"REPORT_INTERVAL"`
 }
 
 func NewConfig() *Config {
